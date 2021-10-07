3147fc4f96b8a148e76db191a620aee522a158e2
Kerim Njuhovic <kerimnjuhovic@gmail.com>
2021-10-07 12:06:09 +0200

    Project Architecture

---
+++ News/News/View/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Kerim Njuhovic on 7. 10. 2021..
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Homepage()
+        }
+    }
+}
